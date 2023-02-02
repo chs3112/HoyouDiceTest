5b2a9c01af2a4cd1bc4fc7b78166e10ca5580b98
Choi HoSung <chs3112a@gmail.com>
2023-02-02 22:50:35 +0900

    죽은 횟수와 공격 횟수가 일치하지 않음

--- Game.py
+++ Game.py
@@ -8,6 +8,23 @@ def Create_Player(game):
     
     return players
 
+class Save:
+    n_rollDice = [0,0,0,0]
+    n_Attack = [0,0,0,0]
+    point = [0,0,0,0]
+    n_travel_distance = [0,0,0,0]
+    n_die = [0,0,0,0]
+
+    
+    @staticmethod
+    def nprint():
+        for i in range(4):
+            print(f"{i}\n굴린 횟수 : {Save.n_rollDice[i]/100}\
+                \n공격 횟수 : {Save.n_Attack[i]/100}\
+                \n획득 점수 : {Save.point[i]/100}\
+                \n이동 거리 : {Save.n_travel_distance[i]/100}\
+                \n죽은 횟수 : {Save.n_die[i]/100}")
+
 
 class Game:
     def __init__(self):
@@ -29,11 +46,19 @@ class Game:
         self.players[self.postPlayer].TurnStart();
         
     def GameEnd(self):
-        print("gameEnd")
+        for i in range(4):
+            Save.n_rollDice[i] += self.players[i].n_rollDice
+            Save.n_Attack[i] += self.players[i].n_attack
+            Save.point[i] += self.players[i].point
+            Save.n_travel_distance[i] += self.players[i].n_travel_distance
+            Save.n_die[i] += self.players[i].n_die
             
 
 def main():
-    Game()
+    for i in range(100):
+        Game()
+    Save.nprint()
+        
 
 
 if __name__ == '__main__':
--- Player.py
+++ Player.py
@@ -11,15 +11,24 @@ class Player:
         self.point = 0
         self.is_reStart = False
         self.game = game
+        self.is_origin_protection = True
+        
+        self.n_rollDice = 0
+        self.n_attack = 0
+        self.n_travel_distance = 0
+        self.n_die = 0
         
     # main
     def TurnStart(self):
+        self.is_origin_protection = False
         self.RollDice()
         
         
     # main
     def RollDice(self):
+        self.n_rollDice += 1
         self.dice = rd.randrange(1,7)
+        self.n_travel_distance += self.dice
         self.Moves(self.dice)
         
     # main
@@ -64,14 +73,21 @@ class Player:
             self.game.Next(self.sequence)
             
     def Attack(self):
-        self.tile.player.Die()
-        self.AddPoint(1)
+        if self.tile.player.Die():
+            self.n_attack+= 1
+            self.AddPoint(1)
             
     def Die(self):
-        self.tile.player = None
-        self.location = self.sequence*10
-        self.tile = Map.tiles[self.location]
-        self.AddPoint(-1)
+        if not self.is_origin_protection:
+            self.is_origin_protection = True
+            self.n_die += 1
+            self.tile.player = None
+            self.location = self.sequence*10
+            self.tile = Map.tiles[self.location]
+            self.AddPoint(-1)
+            return True
+        else:
+            return False
         
     def ReStart(self):
         self.is_reStart = False
